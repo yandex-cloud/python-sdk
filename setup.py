f84431c5f897ee51def3c4095166974f5d74b72b
semantic-release <semantic-release>
2023-02-14 04:03:34 +0000

    0.196.0
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 packages = find_packages(".", include=["yandexcloud*", "yandex*"])
 
-__version__ = "0.195.0"
+__version__ = "0.196.0"
 
 setup(
     name="yandexcloud",
