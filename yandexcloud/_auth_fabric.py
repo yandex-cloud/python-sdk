78f38406358db8bb4b015c1927a6781fb1c11428
Dmitry Chumak <dmchumak@yandex-team.ru>
2024-04-23 17:08:02 +0300

    first draft

--- yandexcloud/_auth_fabric.py
+++ yandexcloud/_auth_fabric.py
@@ -72,6 +72,18 @@ def get_auth_token_requester(
     raise RuntimeError("Unknown auth method: {}".format(auth_name))
 
 
+def get_iam_token(service_account_key=None):
+    token_requester = get_auth_token_requester(service_account_key=service_account_key)
+    if not service_account_key:
+        return token_requester.get_token()
+
+    from . import SDK
+    from yandex.cloud.iam.v1.iam_token_service_pb2_grpc import IamTokenServiceStub
+    sdk = SDK()
+    client = sdk.client(IamTokenServiceStub)
+    return client.Create(token_requester.get_token_request()).iam_token
+
+
 class MetadataAuth:
     def __init__(self, metadata_addr):
         self.__metadata_addr = metadata_addr
