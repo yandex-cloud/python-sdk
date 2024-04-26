64a793592155bec26452c04d63c86eda1e80236f
Dmitry Chumak <dmchumak@yandex-team.ru>
2024-04-26 14:25:33 +0300

    some fixes

--- yandexcloud/auth.py
+++ yandexcloud/auth.py
@@ -1,15 +1,14 @@
 from yandex.cloud.iam.v1.iam_token_service_pb2_grpc import IamTokenServiceStub
 from yandexcloud import SDK
-from yandexcloud._auth_fabric import get_auth_token_requester
+from yandexcloud._auth_fabric import ServiceAccountAuth, get_auth_token_requester
 
 
-def get_iam_token_by_metadata():
-    return get_auth_token_requester().get_token()
+def get_iam_token_by_metadata(metadata_addr=None):
+    return get_auth_token_requester(metadata_addr=metadata_addr).get_token()
 
 
 def get_iam_token_by_sa_key(service_account_key):
-    token_requester = get_auth_token_requester(service_account_key=service_account_key)
-
     sdk = SDK()
     client = sdk.client(IamTokenServiceStub)
-    return client.Create(token_requester.get_token_request()).iam_token
+    sa_auth = ServiceAccountAuth(sa_key=service_account_key)
+    return client.Create(sa_auth.get_token_request()).iam_token
