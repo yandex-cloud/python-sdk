adbae184f44e951b977f77ebfea4b51295674c55
Dmitry Chumak <dmchumak@yandex-team.ru>
2024-04-26 16:30:55 +0300

    added `endpoint` arg to `get_auth_token()`

--- yandexcloud/auth.py
+++ yandexcloud/auth.py
@@ -1,10 +1,10 @@
 from yandex.cloud.iam.v1.iam_token_service_pb2_grpc import IamTokenServiceStub
 from yandexcloud import SDK
-from yandexcloud._auth_fabric import MetadataAuth, get_auth_token_requester
+from yandexcloud._auth_fabric import MetadataAuth, get_auth_token_requester, YC_API_ENDPOINT
 
 
-def get_auth_token(token=None, service_account_key=None, iam_token=None, metadata_addr=None):
-    requester = get_auth_token_requester(token, service_account_key, iam_token, metadata_addr)
+def get_auth_token(token=None, service_account_key=None, iam_token=None, metadata_addr=None, endpoint=YC_API_ENDPOINT):
+    requester = get_auth_token_requester(token, service_account_key, iam_token, metadata_addr, endpoint=endpoint)
     if isinstance(requester, MetadataAuth):
         return requester.get_token()
 
